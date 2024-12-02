355c22ae409ca9c28ac8ed15002cc1ade1cec057
Yariaz0801 <designsbyzairay@gmail.com>
2024-12-02 14:31:43 -0500

    Post Lunch Review with Prepared Statements

--- Wb8demo2/src/main/java/com/pluralsight/Main.java
+++ Wb8demo2/src/main/java/com/pluralsight/Main.java
@@ -1,45 +1,64 @@
 package com.pluralsight;
 
 import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        try{
-            // load the MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.wb8demo2 <username> <password>");
+            System.exit(1);
+        }
 
-            // 1. open a connection to the database
-            // use the database URL to point to the correct database
-            Connection connection;
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/sakila",
-                    "newuser",
-                    "yearup");
-            // create statement
-            // the statement is tied to the open connection
-            Statement statement = connection.createStatement();
-            // define your query
-            String query = "SELECT * FROM sakila.city WHERE country_id = 103;";
-            // 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
-            // process the results
-            while (results.next()) {
-                String city = results.getString("city");
-                System.out.println(city);
+        try {
+            // get the user name and password from the command line args
+            String username = args[0];
+            String password = args[1];
+
+            try {
+
+                // load the MySQL Driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                // 1. open a connection to the database
+                // use the database URL to point to the correct database
+                Connection connection;
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/sakila",
+                        "newuser",
+                        "yearup");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", username, password);
+
+                int country_id = 103;
+
+                PreparedStatement pStatement = connection.prepareStatement("SELECT * FROM sakila.city WHERE country_id = ?;");
+                pStatement.setInt(1, country_id);
+
+
+                ResultSet results = pStatement.executeQuery();
+                // process the results
+                while (results.next()) {
+                    int cityId = results.getInt("city_id");
+                    String city = results.getString("city");
+                    System.out.println(cityId);
+                    System.out.println(city);
+                }
+                // 3. Close the connection
+                results.close();
+                pStatement.close();
+                connection.close();
+
+            } catch (ClassNotFoundException e) {
+                System.out.println("There was an issue finding a class:");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println("There was an SQL issue:");
+                e.printStackTrace();
             }
-            // 3. Close the connection
-            connection.close();
 
 
-            System.out.println("Hello world!");
-        } catch (ClassNotFoundException e){
-            System.out.println("There was an issue finding a class:");
-            e.printStackTrace();
         }
-        catch (SQLException e) {
-            System.out.println("There was an SQL issue:");
-            e.printStackTrace();
-        }
-
     }
 }
